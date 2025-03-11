2d829e75777b51c955808ba34a3ec418ecd1894d
孙子文 <sunziwens@gmail.com>
2025-03-11 17:55:19 +0800

    rollback to java8

--- code/src/test/java/com/withoutcat/petwish/PetwishApplicationTests.java
+++ code/src/test/java/com/withoutcat/petwish/PetwishApplicationTests.java
@@ -1,10 +1,14 @@
 package com.withoutcat.petwish;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.withoutcat.petwish.controller.HealthzController;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PetwishApplicationTests {
   @Test
